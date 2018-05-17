85d00e76a169cc033723c4af893f8bed9cfee50a
yueransutd <1055538755@qq.com>
2018-05-17 16:41:18 +0800

    update pad_label

--- NER-pytorch-master/pad_label.py
+++ NER-pytorch-master/pad_label.py
@@ -1,14 +1,35 @@
-with open("./dataset/single_out.txt" , "r", encoding="utf8") as f:
-    a= f.readlines()
-    b=[]
-    oup = ""
-    for word in a:
+
+'''
+input: single column of word
+output: four columns
+'''
+def extend_label():
+    with open("./dataset/single_out.txt" , "r", encoding="utf8") as f:
+        a= f.readlines()
+        b=[]
+        oup = ""
+        for word in a:
         
-        if len(word)>=1 and word!=" ":
-            w = word[:-1]+" NNP I-NP I-ORG"
-            if len(w)!= 15:
-                b.append(w)
-    for ww in b:
-        oup+= ww+"\n"
-with open("./dataset/new_single_out.txt", "w", encoding = "utf8") as fi:
-    fi.write(oup)
+            if len(word)>=1 and word!=" ":
+                w = word[:-1]+" NNP I-NP I-ORG"
+                if len(w)!= 15:
+                    b.append(w)
+        for ww in b:
+            oup+= ww+"\n"
+    with open("./dataset/new_single_out.txt", "w", encoding = "utf8") as fi:
+        fi.write(oup)
+
+def extend_middle_label():
+    oup = ""
+    with open("./dataset/test.txt" , "r", encoding="utf8") as f:
+        a= f.readlines()
+    for line in a:
+        new_line = " ".join([line.split()[0]+ " NNP I-NP "+line.split()[1]])
+        oup+=new_line+"\n"
+
+    with open("./dataset/new_test.txt", "w", encoding = "utf8") as fi:
+        fi.write(oup)
+
+extend_middle_label()
+
+
